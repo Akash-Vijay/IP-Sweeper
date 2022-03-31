9a572c2d8adff43ddc1a2add7236b8cbc1bc3771
Akash Vijay <akashvijay789@gmail.com>
2022-03-31 21:42:13 +0530

    Added exception handling

--- ipsweeper.sh
+++ ipsweeper.sh
@@ -4,11 +4,20 @@
 
 * Creator       :  Akash Vijay
 * Description   :  Script to find all active IP Addresses in a network
-* Version       :  1.0
+* Version       :  1.1
 
 COMMENT
 
 
+if ["$1" == ""]
+then
+echo ""
+echo "Invalid Synatx   "
+echo "Expected Synatx  : ./ipsweeper.sh {First 3 section of IP Address}"
+echo "Sample Syntax    : ./ipsweeper.sh 192.168.72"
+
+else
 for ip in `seq 1 254`;do
 ping $1.$ip -c 1 | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
 done
+fi
